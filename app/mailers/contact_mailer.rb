82dabc8cbc29472d89a7b2b6c0bcdf5c5c6af179
Teo Roman <teo_romann@yahoo.com>
2015-07-02 14:04:42 +0000

    Modify contact_mailer code

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ActionMailer::Base
     default to: 'teo_romann@yahoo.com'
-    def contact_mailer(name, email, body)
+    def contact_email(name, email, body)
         @name = name
         @email = email
         @body  = body
